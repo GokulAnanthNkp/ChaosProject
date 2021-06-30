c16918dd01ff6cf5e2756918e241b643839e6363
harshal1711 <harshal.sable1711@gmail.com>
2021-06-30 12:04:04 +0300

    Restructured folder(added html folder)

--- app.js
+++ app.js
@@ -13,30 +13,6 @@ app.use('/', router)
 
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-
-// app.use('/dist', express.static(path.join(__dirname, 'dist')))
-// app.use('/plugins', express.static(path.join(__dirname, 'plugins')))
-// app.get('/',(req,res) => {
-//     res.sendFile(__dirname + '/index.html');
-// })
-
-// app.get('/auth',(req,res) => {
-//     res.sendFile(__dirname + '/auth.html');
-// })
-
-    /** 
- app.post('/auth.html',(req,res)=>{
-     const user = {
-         ak = req.body.ak,
-         sak = req.body.sak,
-         rn = req.body.rn
-        };
-        res.cookie("user",user);
-        res.redirect('index.html');
-    })
-    */
-
-
 const port = 5008;
 
 app.listen(port,function(err){
--- router.js
+++ router.js
@@ -4,29 +4,51 @@ const path = require('path');
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname , 'public', 'home.html'))
+    res.sendFile(path.join(__dirname , 'public', 'html', 'home.html'))
 })
 
-router.get('/auth', (req, res) => {
-    res.sendFile(path.join(__dirname , 'public', 'auth.html'))
+router.get('/aws/auth', (req, res) => {
+    res.sendFile(path.join(__dirname , 'public', 'html', 'auth.html'))
 })
 
-router.post('/auth', (req, res) => {
+router.post('/aws/auth', (req, res) => {
     console.log(req.body.access_key)
     console.log(req.body.secret_access_key)
     console.log(req.body.region)
-    res.redirect('/auth_suc')
+    res.redirect('/aws/auth_success')
     // console.log(req.body.service)
-    // res.sendFile(path.join(__dirname , 'public', 'auth_suc2.html'))
+    // res.sendFile(path.join(__dirname , 'public', 'html', 'auth_suc2.html'))
 })
 
-router.get('/auth_suc', (req, res) => {
-    res.sendFile(path.join(__dirname , 'public', 'auth_suc1.html'))
+router.get('/aws/auth_success', (req, res) => {
+    res.sendFile(path.join(__dirname , 'public', 'html', 'auth_success.html'))
 })
 
-router.post('/auth_suc', (req, res) => {
+router.post('/aws/auth_success', (req, res) => {
     console.log(req.body.service)
-    res.sendFile(path.join(__dirname , 'public', 'auth_suc1.html'))
+    if(req.body.service === 'ec2'){
+        res.redirect('/aws/ec2')
+    }
+    if(req.body.service === 'ecs'){
+        res.redirect('/aws/ecs')
+    }
+})
+
+router.get('/aws/ec2', (req, res) => {
+    res.sendFile(path.join(__dirname , 'public', 'html', 'ec2.html'))
+})
+
+router.post('/aws/ec2', (req, res) => {
+    console.log(req.body.module)
+    if(req.body.module === 'stop_instances'){
+        res.sendFile(path.join(__dirname , 'public', 'html', 'ec2_stop.html'))
+    }
+    if(req.body.module === 'terminate_instances'){
+        res.sendFile(path.join(__dirname , 'public', 'html', 'ec2_terminate.html'))
+    }
+    if(req.body.module === 'start_instances'){
+        res.sendFile(path.join(__dirname , 'public', 'html', 'ec2_start.html'))
+    }
 })
 
 
